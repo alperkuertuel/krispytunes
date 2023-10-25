8b3bb3a6a0b8a7c336ca72eb49b2f194c7f31bb0
abc <abc@example.com>
2023-10-25 15:03:45 +0200

    upgrade header css and semantic

--- modules/header/header.php
+++ modules/header/header.php
@@ -1,7 +1,4 @@
-<div id="header">
-    <a href="index"><img src="img/header.jpg" class="back" alt="krispytunes"/></a>
-    <br />
-    <div id="slogan">
-        KRISPYTUNES - INSTRUMENTALS
-    </div>
-</div>
+<header class="header">
+    <a href="index"><img class="header__image" src="img/header.jpg" alt="krispytunes"/></a>
+    <h1 class="header__slogan">krispytunes - instrumental store</h1>
+</header>
