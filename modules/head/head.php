37e7f7d8b2224a3d05179dbca3c01416af208fff
abc <abc@example.com>
2023-10-25 12:50:04 +0200

    completely restructure head for loadin purposes and page performance

--- modules/head/head.php
+++ modules/head/head.php
@@ -1,19 +1,12 @@
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3DRE0P6CM3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-3DRE0P6CM3');
-    </script>
-    <script src="jquery-3.5.0.min.js"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#2f3640" />
     <meta name="robots" content="all" />
 	<title>krispytunes - INSTRUMENTAL STORE</title>
 	<meta name="description" content="Instrumentals For Sale | Freebeats | Custom Instrumentals | Beats For Commercial Videos" />
 	<meta name="page-topic" content="QUALITY INSTRUMENTALS FOR SALE | INSTANT DELIVERY | FREE DOWNLOAD INSTRUMENTALS WITHOUT VOICE TAG" />
-	<meta name='keywords' content="krispytunes, kirpsytunes.com, instrumental, freebeat, free instrumental, free instrumentals download, freebeat download, rap, without voicetag,
- freebeats download, type beat, type of beat, hiphop beat, hiphop instrumental free, hiphop instrumentals free, hiphop beats free, hiphop beat free, hiphopmusic, rap beat, scratch instrumental, freebeat rap, panda beat" />
+	<meta name='keywords' content="krispytunes, kirpsytunes.com, instrumental, freebeats, free instrumentals, free instrumentals download, freebeats download, beats without voicetag" />
 	<meta name="author" content="krispytunes" />
 	<meta name="copyright" content="krispytunes">
 	<meta name='Classification' content='Business' />
@@ -21,25 +14,26 @@
     <meta property="og:image:type" content="image/jpeg">
     <meta property="og:image:width" content="200">
     <meta property="og:image:height" content="200">
-    <meta name="google-site-verification" content="p1Ihv-yEo78bVZQqBVqkVXwGfICG3GvsICcTqRXMF6Y" />
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta name="theme-color" content="#2f3640" />
-	
-    <link type="image/ico" rel="icon" href="favicon.ico"/>
+    <link type="image/ico" rel="icon" href="favicon.ico">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
+    <!-- Animation: "KRISPYTUNES - INSTRUMENTALS -->
+    <link rel="preconnect" href="https://cdnjs.cloudflare.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+    <!-- fontawesome -->
     <link rel="stylesheet" href="./fontawesome/font-awesome.min.css">
 
+    <!-- stylesheet, fontAwesome and jQuery -->
+    <link href="stylesheet_v11.css" rel="stylesheet" type="text/css">
+    <script src="jquery-3.5.0.min.js"></script>
+
+    <!-- Flickity -->
     <link rel="stylesheet" href="./flickity/flickity.min.css">
     <script src="./flickity/flickity.pkgd.min.js" defer></script>
 
-    <!-- Source for animation of "KRISPYTUNES - INSTRUMENTALS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-
-    <link href="stylesheet_v11.css" rel="stylesheet" type="text/css" />
-
+    <!-- Airbit -->
     <script async>
         function loadAirbitPlayer() {
             const iframe = document.createElement("iframe");
@@ -50,20 +44,28 @@
         }
     </script>
 
-    <script src="https://www.google.com/recaptcha/api.js" defer></script>
+    <!-- Google: reCaptcha -->
+    <script src="https://www.google.com/recaptcha/api.js" async></script>
     <script src="https://www.google.com/recaptcha/api.js?render=6Lc74uQZAAAAALiJPavxE5e2X5iTltduKn-mYYCo"></script>
-    
     <script>
             grecaptcha.ready(function() {
                 grecaptcha.execute('6Lc74uQZAAAAALiJPavxE5e2X5iTltduKn-mYYCo', { action: 'submit' }).then(function(token) {
                     document.getElementById('recaptchaResponse').value = token;
                 });
             });
-
             grecaptcha.ready(function() {
                 grecaptcha.execute('6Lc74uQZAAAAALiJPavxE5e2X5iTltduKn-mYYCo', { action: 'submit' }).then(function(token) {
                     document.getElementById('recaptchaResponse2').value = token;
                 });
             });
+    </script>
+
+    <!-- Google: Tag-Manager -->
+    <script src="https://www.googletagmanager.com/gtag/js?id=G-3DRE0P6CM3" async></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-3DRE0P6CM3');
     </script>
 </head>
