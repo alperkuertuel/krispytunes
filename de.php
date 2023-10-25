24d29408a8366f871ffeb94c411eccba13a06973
abc <abc@example.com>
2023-10-25 15:36:16 +0200

    create semantic body - header - main - footer

--- de.php
+++ de.php
@@ -1,45 +1,27 @@
 <!doctype html>
     <html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
-	<?php include './modules/head/head-de.php'; ?>
+	<?php include './modules/head/head.php'; ?>
 	<body>
-        <nav class="shift">
-            <!--googleoff: snippet-->
-            <div id="flags">
-                <a href="index"><img src="img/us.svg" alt="us"/></a> 
-                <a href="de"><img src="img/de.svg" alt="de"/></a>
-            </div>
-            <!--googleon: snippet-->
-            <div class="container" onclick="myFunction(this)">
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
-            </div>
-            <ul class="showing">
-                <li><a href="#header">INSTRUMENTAL STORE</a></li>
-                <li><a href="#Licenses">LIZENZEN</a></li>
-                <li><a href="#Services">SERVICES</a></li>
-                <li><a href="#Freebeats">FREEBEATS</a></li>
-                <li><a href="#Contact">KONTAKT</a></li>
-            </ul>
-        </nav>
         <?php include './modules/header/header-de.php'; ?>
-        <?php include './modules/store/store-de.php'; ?>
-        <?php include './modules/clients/clients-de.php'; ?>
-        <?php include './modules/license-terms/license-terms-de.php'; ?>
-        <?php include './modules/services/services-de.php'; ?>
-        <?php include './modules/freebeats/freebeats-de.php'; ?>
-        <?php /* include '/modules/faq/faq-de.php'; */ ?>
-        <!--googleoff: snippet-->
-        <?php include './modules/contact/contact-de.php'; ?>
+        <main class="main">
+            <?php include './modules/store/store-de.php'; ?>
+            <?php include './modules/clients/clients-de.php'; ?>
+            <?php include './modules/license-terms/license-terms-de.php'; ?>
+            <?php include './modules/services/services-de.php'; ?>
+            <?php include './modules/freebeats/freebeats-de.php'; ?>
+            <?php /* include '/modules/faq/faq-de.php'; */ ?>
+            <!--googleoff: snippet-->
+            <?php include './modules/contact/contact-de.php'; ?>
+            <div class="cookie-container">
+                <p>
+                    Diese Seite verwendet Cookies, um die Benutzerfreundlichkeit zu verbessern. Wenn Sie meine Website nutzen, gehe ich davon aus, dass Sie mit der <a href="privacy-policy" target="_blank">Datenschutzerklärung</a> einverstanden sind.
+                    <br />
+                    <br />
+                    <span style="font-size: .8em;">Diese Seite ist durch Google reCaptcha geschützt. Somit gilt die <a href="https://policies.google.com/privacy">Datenschutzerklärung</a> und diese <a href="https://policies.google.com/terms">Geschäftsbedingungen</a>.</span>
+                </p>
+                <button class="cookie-btn">OK, WEITER</button>
+            </div>
+        <main>
         <?php include './modules/footer/footer-de.php'; ?>
-        <div class="cookie-container">
-            <p>
-                Diese Seite verwendet Cookies, um die Benutzerfreundlichkeit zu verbessern. Wenn Sie meine Website nutzen, gehe ich davon aus, dass Sie mit der <a href="privacy-policy" target="_blank">Datenschutzerklärung</a> einverstanden sind.
-                <br />
-                <br />
-                <span style="font-size: .8em;">Diese Seite ist durch Google reCaptcha geschützt. Somit gilt die <a href="https://policies.google.com/privacy">Datenschutzerklärung</a> und diese <a href="https://policies.google.com/terms">Geschäftsbedingungen</a>.</span>
-            </p>
-            <button class="cookie-btn">OK, WEITER</button>
-        </div>
     </body>
 </html>
---
+++ modules/nav/nav.php
@@ -0,0 +1,20 @@
+<nav class="shift">
+    <!--googleoff: snippet-->
+    <div id="flags">
+        <a href="index"><img src="img/us.svg" alt="us"/></a>
+        <a href="de"><img src="img/de.svg" alt="de"/></a>
+    </div>
+    <!--googleon: snippet-->
+    <div class="container" onclick="myFunction(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
+    <ul class="showing">
+        <li><a href="#header">INSTRUMENTAL STORE</a></li>
+        <li><a href="#Licenses">LICENSE TERMS</a></li>
+        <li><a href="#Services">SERVICES</a></li>
+        <li><a href="#Freebeats">FREEBEATS</a></li>
+        <li><a href="#Contact">CONTACT</a></li>
+    </ul>
+</nav>
