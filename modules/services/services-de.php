f7ac91ed8a86daacaf4c76fd2d9b668850b5c0d5
alper92 <alperkuertuel@hotmail.de>
2023-12-19 18:09:21 +0100

    seperate services and remove hr

--- modules/services/services-de.php
+++ modules/services/services-de.php
@@ -1,28 +1,27 @@
-<h1 id="Services"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> SERVICES</h1>
-<hr>
-    <div class="flex">
-        <div  itemscope itemtype="http://schema.org/Dataset"  id="left">
-            <h1 itemprop="name">CUSTOM BEATS</h1>
-            <img src="img/custom.svg" alt="CUSTOM INSTRUMENTALS" loading="lazy"/>
-            <h2 >Suchst du nach etwas Einzigartigem?</h2>
-            <p itemprop="description">
-                Dann <a href="#cont">kontaktiere </a> mich, um einen auf dich zugeschnittenen Beat zu bekommen.
-            </p>
-        </div>
-
-        <div id="right">
-            <h1>BEATS F&Uuml;R WERBEVIDEOS</h1>
-            <img src="img/videos.svg" alt="COMMERCIAL VIDEOS" loading="lazy"/>
-            <p>
-                Suchst du nach einer speziellen Stimmung in deinem Werbevideo?
-            </p>
-            <p>
-                &Uuml;berzeuge dich selbst mit diesen Videos:
-            </p>
-            <p>
-                <a href="https://www.youtube.com/watch?v=NRdXrIWCX_c" target="_blank" rel="noopener">VAUDE</a>
-                <br />
-                <a href="https://www.youtube.com/watch?v=wwDMefv_5nc" target="_blank" rel="noopener">Don't Drop Crossfit</a>
-            </p>
-        </div>
+<section class="flex">
+    <h1 id="Services"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> SERVICES</h1>
+    <div  itemscope itemtype="http://schema.org/Dataset"  id="left">
+        <h1 itemprop="name">CUSTOM BEATS</h1>
+        <img src="img/custom.svg" alt="CUSTOM INSTRUMENTALS" loading="lazy"/>
+        <h2 >Suchst du nach etwas Einzigartigem?</h2>
+        <p itemprop="description">
+            Dann <a href="#cont">kontaktiere </a> mich, um einen auf dich zugeschnittenen Beat zu bekommen.
+        </p>
     </div>
+
+    <div id="right">
+        <h1>BEATS F&Uuml;R WERBEVIDEOS</h1>
+        <img src="img/videos.svg" alt="COMMERCIAL VIDEOS" loading="lazy"/>
+        <p>
+            Suchst du nach einer speziellen Stimmung in deinem Werbevideo?
+        </p>
+        <p>
+            &Uuml;berzeuge dich selbst mit diesen Videos:
+        </p>
+        <p>
+            <a href="https://www.youtube.com/watch?v=NRdXrIWCX_c" target="_blank" rel="noopener">VAUDE</a>
+            <br />
+            <a href="https://www.youtube.com/watch?v=wwDMefv_5nc" target="_blank" rel="noopener">Don't Drop Crossfit</a>
+        </p>
+    </div>
+</section>
--- modules/services/services.php
+++ modules/services/services.php
@@ -1,29 +1,26 @@
-<h1 id="Services"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> SERVICES</h1>
-<hr>
-    <div class="flex">
-        <div  itemscope itemtype="http://schema.org/Dataset"  id="left">
-            <h1 itemprop="name">ORDER YOUR CUSTOM BEAT NOW</h1>
-            <img src="img/custom.svg" alt="CUSTOM INSTRUMENTALS" loading="lazy"/>
-            <h2>Are you looking for a unique beat?</h2>
-            <p itemprop="description">
-                Then <a href="#cont">contact</a> me to get your custom instrumental!
-                I'm going to start from scratch to deliver you an unique and 100% handmade beat.
-            </p>
-        </div>
-
-        <div id="right">
-            <h1>BEATS FOR COMMERCIAL VIDEOS</h1>
-            <img src="img/videos.svg" alt="COMMERCIAL VIDEOS" loading="lazy"/>
-            <p>
-                Are you looking for a special vibe in your videos?
-            </p>
-            <p>
-                <a href="#cont">Get at me</a> and convince yourself with the commercial videos below!
-            </p>
-            <p>
-                <a href="https://www.youtube.com/watch?v=NRdXrIWCX_c" target="_blank" rel="noopener">VAUDE</a>
-                <br />
-                <a href="https://www.youtube.com/watch?v=wwDMefv_5nc" target="_blank" rel="noopener">Don't Drop Crossfit </a>
-            </p>
-        </div>
+<section class="flex">
+    <h1 id="Services"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> SERVICES</h1>
+    <div  itemscope itemtype="http://schema.org/Dataset"  id="left">
+        <h1 itemprop="name">ORDER YOUR CUSTOM BEAT NOW</h1>
+        <img src="img/custom.svg" alt="CUSTOM INSTRUMENTALS" loading="lazy"/>
+        <h2>Are you looking for a unique beat?</h2>
+        <p itemprop="description">
+            Then <a href="#cont">contact</a> me to get your custom instrumental!
+            I'm going to start from scratch to deliver you an unique and 100% handmade beat.
+        </p>
     </div>
+
+    <div id="right">
+        <h1>BEATS FOR COMMERCIAL VIDEOS</h1>
+        <img src="img/videos.svg" alt="COMMERCIAL VIDEOS" loading="lazy"/>
+        <p> Are you looking for a special vibe in your videos?</p>
+        <p>
+            <a href="#cont">Get at me</a> and convince yourself with the commercial videos below!
+        </p>
+        <p>
+            <a href="https://www.youtube.com/watch?v=NRdXrIWCX_c" target="_blank" rel="noopener">VAUDE</a>
+            <br />
+            <a href="https://www.youtube.com/watch?v=wwDMefv_5nc" target="_blank" rel="noopener">Don't Drop Crossfit </a>
+        </p>
+    </div>
+</section>
