1fc7289570afc0db2ffd9666de8b8951e973dd3d
alper92 <alperkuertuel@hotmail.de>
2023-12-19 18:05:06 +0100

    create contact section with h1 in it

--- modules/contact/contact-de.php
+++ modules/contact/contact-de.php
@@ -1,6 +1,4 @@
-<h1 id="Contact"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> KONTAKT</h1>
-<hr>
 <section id="cont">
-    <h2>Lass uns zusammenarbeiten...</h2>
+    <h1 id="Contact"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> KONTAKT</h1>
         <?php include './modules/contact/contactfields-de.php'; ?>
 </section>
--- modules/contact/contact.php
+++ modules/contact/contact.php
@@ -1,6 +1,4 @@
-<h1 id="Contact"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> CONTACT</h1>
-<hr>
 <section id="cont">
-    <h2>Let's work together...</h2>
-        <?php include './modules/contact/contactfields.php'; ?>
+    <h1 id="Contact"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> CONTACT</h1>
+    <?php include './modules/contact/contactfields.php'; ?>
 </section>
