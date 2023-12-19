480bcf7fdf184039b879cd4aaf93abfc0afbc089
alper92 <alperkuertuel@hotmail.de>
2023-12-19 19:44:48 +0100

    delete media-queries and add contact id

--- modules/contact/contact.php
+++ modules/contact/contact.php
@@ -1,4 +1,4 @@
-<section id="cont">
-    <h1 id="Contact"><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> CONTACT</h1>
+<section id="Contact">
+    <h1><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> CONTACT</h1>
     <?php include './modules/contact/contactfields.php'; ?>
 </section>
