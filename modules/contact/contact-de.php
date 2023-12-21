81960196657e49d77b789f1377fe6237c9004334
alper92 <alperkuertuel@hotmail.de>
2023-12-21 20:14:21 +0100

    clean up stylesheet

--- modules/contact/contact-de.php
+++ modules/contact/contact-de.php
@@ -1,4 +1,4 @@
-<section id="Contact">
+<section id="Contact" class="contact">
     <h1><a href="#Instrumental-Store"><img src="img/up.svg" alt="&and;" loading="lazy"/></a> KONTAKT</h1>
     <?php include './modules/contact/contactfields-de.php'; ?>
 </section>
