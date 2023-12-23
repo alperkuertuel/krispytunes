7042616df7365a51cb8e659e7fc4c69c1d9818f7
alper92 <alperkuertuel@hotmail.de>
2023-12-23 22:41:10 +0300

    move nav component into header like semantic structure

--- components/header/header.php
+++ components/header/header.php
@@ -1,5 +1,5 @@
 <header class="header" id="header">
     <a href="index"><img class="header__image" src="img/header.jpg" alt="krispytunes"/></a>
     <h1 class="header__slogan">krispytunes - instrumental store</h1>
-    <?php include './modules/nav/nav.php'; ?>
+    <?php include 'nav/nav.php'; ?>
 </header>
