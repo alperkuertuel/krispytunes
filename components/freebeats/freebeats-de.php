62cf2a24cf73cb7bea2d53038850d32006254b42
alper92 <alperkuertuel@hotmail.de>
2023-12-26 09:58:41 +0100

    move back request form to freebeats.php

--- components/freebeats/freebeats-de.php
+++ components/freebeats/freebeats-de.php
@@ -22,6 +22,25 @@
         <p>
             F&uuml;lle das Formular aus, um die 25 Freebeats im MP3-Format zugesendet zu bekommen.
         </p>
-        <?php include './components/freebeats/request-de.php'; ?>
+        <p data-js="response-message">
+            <!-- response message after submission -->
+        </p>
+        <div data-js="form-container">
+            <form data-js="request-form" method="post" class="freebeats__request-form">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" autocomplete="on" maxlength="30" size="25" required>
+                
+                <label for="email">E-Mail</label>
+                <input type="email" name="email" id="email" autocomplete="on" maxlength="40" size="25" required>
+
+                <label for="freebeats-subscriber">
+                    <input type="checkbox" value="Dieser Kontakt hat dich abonniert." name="freebeats-subscriber" id="freebeats-subscriber" class="subscribe_box">
+                    Ja, ich will auch &uuml;ber Updates informiert werden!
+                </label>
+                
+                <input type="hidden" name="recaptchaResponse" id="recaptchaResponse">
+                <button type="submit">ANFRAGEN</button>
+            </form>
+        </div>
     </article>
 </section>
