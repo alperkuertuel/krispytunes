a2d31f75526300c5d0d5d84d1920f0a8c04b231c
alper92 <alperkuertuel@hotmail.de>
2024-03-31 15:47:49 +0200

    switch places of freebeats articles

--- components/freebeats/freebeats.php
+++ components/freebeats/freebeats.php
@@ -5,17 +5,6 @@
         </a>
         MORE THEN 30 FREEBEATS WITHOUT VOICE TAGS
     </h1>
-    <article class="freebeats__lost-beats">
-        <h2>LOST BEATS VOL. 1</h2>
-        <img src="img/lost_beats_vol_1.png" alt="lost beats vol. 1" loading="lazy"/>
-        <p>
-            Buy any beat from the store and receive the lost beats tape vol. 1 with <b>11 beats in wav-format for free on top!</b>
-        </p>
-        <p>
-            I'm going to send you the 11 instrumentals on your purchasing e-mail right after your purchase. Listen to all 11 beats with a hit on the button below:
-        </p>
-        <a class="freebeats__lost-beats__playlist-button" href="https://www.youtube.com/watch?v=GFTXUxopAfo&list=PLk9TudZPTLnF8vPcMYroRju7WfPni9cGE&index=2" rel="noopener" target="_blank">YOUTUBE-PLAYLIST</a>
-    </article>
     <article class="freebeats__kriyptunes-vol-1">
         <h2>KRISPYTUNES VOL. 1</h2>
         <img src="img/krispytunes_vol_1.png" alt="krispytunes vol 1" loading="lazy"/>
@@ -46,5 +35,16 @@
                 <button type="submit">REQUEST</button>
             </form>
         </div>
+    </article>
+    <article class="freebeats__lost-beats">
+        <h2>LOST BEATS VOL. 1</h2>
+        <img src="img/lost_beats_vol_1.png" alt="lost beats vol. 1" loading="lazy"/>
+        <p>
+            Buy any beat from the store and receive the lost beats tape vol. 1 with <b>11 beats in wav-format for free on top!</b>
+        </p>
+        <p>
+            I'm going to send you the 11 instrumentals on your purchasing e-mail right after your purchase. Listen to all 11 beats with a hit on the button below:
+        </p>
+        <a class="freebeats__lost-beats__playlist-button" href="https://www.youtube.com/watch?v=GFTXUxopAfo&list=PLk9TudZPTLnF8vPcMYroRju7WfPni9cGE&index=2" rel="noopener" target="_blank">YOUTUBE-PLAYLIST</a>
     </article>
 </section>
