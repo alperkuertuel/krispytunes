0c47b14a1611a94471d4ca34c4d8e6b17c966561
alper92 <alperkuertuel@hotmail.de>
2024-04-01 14:13:18 +0200

    fixes german cookie container class

--- components/cookie-container/cookie-container-de.php
+++ components/cookie-container/cookie-container-de.php
@@ -3,7 +3,7 @@
         Diese Seite verwendet analytische Cookies. Wenn Sie meine Website nutzen, gehe ich davon aus, dass Sie mit der <a href="privacy-policy" target="_blank">Datenschutzerklärung</a> einverstanden sind.
     </p>
     <p class="cookie-container__recaptcha-text">Diese Seite ist durch Google reCaptcha geschützt. Somit gilt die <a href="https://policies.google.com/privacy">Datenschutzerklärung</a> und diese <a href="https://policies.google.com/terms">Geschäftsbedingungen</a>.</p>
-    <div class="cookie-container__button_container">
+    <div class="cookie-container__button-container">
         <button class="cookie-container__button" data-js="accept-cookie-button">Akezptieren</button>
         <button class="cookie-container__button" data-js="decline-cookie-button">Ablehnen</button>
     </div>
