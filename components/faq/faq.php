f70f6ecf74a6c0ee68ad3fc2b77e32e88fe603eb
alperkuertuel <alperkuertuel@hotmail.de>
2024-12-31 09:48:07 +0100

    adds accorfaq accordion

---
+++ components/faq/faq.php
@@ -0,0 +1,67 @@
+<h1>
+    <a href="#Instrumental-Store" aria-label="scroll up to instrumental store">
+        <img src="/img/fa-icons/angle-up-solid.svg" class="fa-icon" aria-label="scroll to top" />
+    </a>
+    FAQ
+</h1>
+<section class="accordion">
+    <details class="accordion__item">
+        <summary class="accordion__item-title">What does a purchase include?</summary>
+        <p class="accordion__item-content">
+            Each purchase includes a license agreement/receipt stating your rights to the instrumentals, 
+            payment details, etc., and the untagged instrumental as a wav-file & mp3-file. 
+            Wav leasing rights include the stereo-mix of the instrumental(s) in wav-format & mp3-format. 
+            Premium, unlimited, and exclusive rights also include the separated instrumental tracks 
+            of the instrumental(s) in wav-format (also known as "tracked outs" or "stems") for professional mixing. 
+            You will receive these files via download-links sent to your email address. 
+            Please check your spam folder if you do not find the purchasing email in your inbox!
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">Which licenses contain the stems / track outs of the instrumental?</summary>
+        <p class="accordion__item-content">
+            The premium leasing license, the unlimited leasing license, and the exclusive license contain 
+            the stems / track outs. You will receive a zip file and a password to unpack.
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">Do I have to pay for the license again when the license duration is over?</summary>
+        <p class="accordion__item-content">
+            Except for the exclusive license, all licenses have a duration of 3 years starting from the 
+            date you purchased the beats. If you want to prolong your licenses for more years, you must 
+            pay an additional fee. For the next 3 years, you pay the same amount as your initial payment. 
+            To avoid paying fees after the 3-year duration, you can switch to an exclusive license or 
+            cancel the contract under German law (3 months before the end of the leasing contract). 
+            When switching to the EXCLUSIVE License, I will deduct the amount you already paid for the leasing.
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">Do you offer other payment methods than PayPal or Master/Debit card?</summary>
+        <p class="accordion__item-content">
+            YES! I also accept payments from any bank. You will receive a bill (PDF file) via email with all 
+            the information you need. After payment, you will receive the instrumental(s) and rights via direct download link.
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">I own a WAV leasing license. Can I upgrade my license to a higher license?</summary>
+        <p class="accordion__item-content">
+            Please contact me via email. For license upgrades, I will credit the amount you have already paid 
+            and deduct it from the price for the desired license.
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">Can I release a song made on your freebeat on Spotify or monetize it?</summary>
+        <p class="accordion__item-content">
+            Some of the freebeats contain samples, and you must clear the sample first before monetizing the song. 
+            If you plan a profit release (if necessary: after clearing the sample), you must purchase a suitable license.
+        </p>
+    </details>
+    <details class="accordion__item">
+        <summary class="accordion__item-title">How do I give proper credit?</summary>
+        <p class="accordion__item-content">
+            Proper credit is given as follows in written form: "prod. by krispytunes". 
+            Hard copies such as CDs, LPs, etc., need to include written credit in booklets, covers, or labels. 
+            Streaming releases such as Spotify, YouTube, SoundCloud, etc., need to include written credit.
+        </p>
+    </details>
+</section>
